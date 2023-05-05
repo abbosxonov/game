aa4c5ae15fb9521c09d842d17436f1ecae62d334
unknown <tolanboyabbosxonov9@gmail.com>
2023-05-05 11:58:43 +0500

    first commit

--- js/java.js
+++ js/java.js
@@ -11,3 +11,14 @@ function shrink() {
     navbar.classList.remove("navbar-shrink");
   }
 }
+
+let modeBtn = document.getElementById("dark-light");
+
+modeBtn.addEventListener("click", function () {
+  if (document.body.className != "light") {
+    this.firstElementChild.src = "./images/dark.svg";
+  } else {
+    this.firstElementChild.src = "./images/light.svg";
+  }
+  document.body.classList.toggle("light");
+});
